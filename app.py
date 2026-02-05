f5794649e07b066032cf187aa936c23cc6ceee58
tunodarin <g2272062@tcu.ac.jp>
2026-02-05 19:21:41 +0900

    Update app.py and index.html

--- app.py
+++ app.py
@@ -16,12 +16,14 @@ def init_db():
  
 @app.route('/')
 def index():
-     # DBからデータを取得して表示する
     with sqlite3.connect('database.db') as conn:
         conn.row_factory = sqlite3.Row
-        # is_completed を取得
-        # deadlineを取得
-        tasks = conn.execute("SELECT id, title, deadline, is_completed FROM tasks").fetchall()
+        #is_completed を取得
+        #deadlineを取得
+        #deadline IS NULL を使って、期限なしを後ろ(1)、期限ありを前(0)にする
+        # 2. その後、期限があるもの同士を日付順(ASC)に並べる
+        tasks = conn.execute("""SELECT id, title, deadline, is_completed FROM tasks 
+     　　ORDER BY is_completed ASC, deadline IS NULL ASC, deadline ASC""").fetchall()
     return render_template('index.html', tasks=tasks)
 
 @app.route('/add', methods=['POST'])
